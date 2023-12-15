eb0dff7ab1d946a37da12b44a31b75019eeeeec9
Zentrik <Zentrik@users.noreply.github.com>
2023-12-15 15:59:15 +0000

    Day 7 Part 1
    Day 14 part 2 too slow

--- day14.jl
+++ day14.jl
@@ -34,3 +34,63 @@ O.#..O.#.#
 #OO..#....""" |> IOBuffer |> eachline)
 
 part1()
+
+function calculate_load(input)
+    no_of_rows = size(input)[1]
+    total_load = 0
+
+    for column in eachcol(input)
+        next_free_slot = 1
+        for (row, c) in pairs(column)
+            if c == 'O'
+                total_load += no_of_rows - (next_free_slot - 1)
+                next_free_slot += 1
+            elseif c == '#'
+                next_free_slot = row + 1
+            end
+        end
+    end
+    total_load
+end
+
+function north_tilt!(grid)
+    no_of_rows = size(grid)[1]
+
+    @inbounds for (col, column) in pairs(eachcol(grid))
+        next_free_slot = 1
+        for (row, c) in pairs(column)
+            if c == 'O'
+                grid[row, col] = '.'
+                grid[next_free_slot, col] = 'O'
+                next_free_slot += 1
+            elseif c == '#'
+                next_free_slot = row + 1
+            end
+        end
+    end
+end
+
+function part2(io=eachline("day14.txt"))
+    matrix = stack(io; dims=1)
+    # for _ in 1:1000000000-1
+        for i in 1:4
+            @inline north_tilt!(matrix)
+            # display(rotl90(matrix, i-1))
+            @inline matrix = rotr90(matrix)
+        end
+    # end
+    calculate_load(matrix)
+end
+
+part2("""O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....""" |> IOBuffer |> eachline)
+
+part2()
