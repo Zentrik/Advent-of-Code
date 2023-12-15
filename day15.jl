6b6b7bc9b4f66cb237b22666283f20706c098b34
Zentrik <Zentrik@users.noreply.github.com>
2023-12-15 20:26:20 +0000

    Day 15 Part 2

--- day15.jl
+++ day15.jl
@@ -24,10 +24,12 @@ function part1_fast(ascii_str::Vector{UInt8})
     i = 1
     while i <= length(ascii_str)
         hash = 0u8
-        while i <= length(ascii_str) && @inbounds ascii_str[i] != UInt8(',')
-            @inbounds hash += ascii_str[i]
+        c = @inbounds ascii_str[i]
+        while i <= length(ascii_str) && c != ','u8
+            hash += c
             hash *= 17u8
             i += 1
+            c = @inbounds ascii_str[i]
         end
         result += hash
 
@@ -47,7 +49,7 @@ part1()
 @test part1_fast(",HASH" |> Vector{UInt8}) == 52
 @test part1_fast("HASH,HASH" |> Vector{UInt8}) == 104
 @test part1_fast("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7" |> Vector{UInt8}) == 1320
-part1_fast()
+@test part1_fast() == 510801
 
 using BenchmarkTools
 lines = eachsplit(readline("day15.txt"), ',')
@@ -57,3 +59,55 @@ ascii_str = readline("day15.txt") |> Vector{UInt8}
 
 @profview for _ in 1:10^3 part1(lines) end
 @profview for _ in 1:10^3 part1_fast(ascii_str) end
+
+using OrderedCollections
+function part2(io="day15.txt")
+    open(io) do f
+        readuntil(f, UInt8('\n'))
+    end |> part2
+end
+function part2(ascii_str::Vector{UInt8})
+    # boxes = Vector{OrderedDict}(undef, 256)
+    boxes = [OrderedDict() for _ in  1:256]
+
+    i = 1
+    while i <= length(ascii_str)
+        c = @inbounds ascii_str[i]
+        start_i = i
+        idx = 0u8
+        while c ∉ ('='u8, '-'u8)
+            idx += c
+            idx *= 17u8
+
+            i += 1
+            c = @inbounds ascii_str[i]
+        end
+        # if @inbounds !isassigned(boxes, idx+1)
+        #     boxes[idx+1] = OrderedDict()
+        # end
+
+        val = @inbounds @views ascii_str[i+1] - '0'u8
+
+        label = @inbounds ascii_str[start_i:i-1]
+        if c == '='u8
+            boxes[idx+1][label] = val
+            i += 2
+        elseif c == '-'u8
+            delete!(boxes[idx+1], label)
+            i += 1
+        end
+
+        i += 1
+    end
+
+    sum(pairs(boxes)) do (box_num, box)
+        if !isempty(box)
+            box_num * sum(prod, enumerate(values(box)))
+        else
+            0
+        end
+    end
+end
+
+@test 145 == part2("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7" |> Vector{UInt8})
+part2()
