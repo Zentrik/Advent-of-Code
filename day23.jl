93ad3c43d357832bafe5aefe892ace195432b902
Zentrik <Zentrik@users.noreply.github.com>
2023-12-24 00:32:55 +0000

    Day 23 Part 2 ~250ms

--- day23.jl
+++ day23.jl
@@ -85,15 +85,31 @@ function part2(io="day23.txt")
     replace!(map, (('^', '>', 'v', '<') .=> '.')...)
 
     # Create compressed graph
-    adjacency_list = Dict{NTuple{2, Int}, Vector{NTuple{3, Int}}}()
-    process_node!(adjacency_list, map, start_node, goal_node, start_node)
+    idx_to_idx = Dict{NTuple{2, Int}, Int}() # to compress adjacency_list into vector
+    adjacency_list = NTuple{4, NTuple{2, Int}}[]
+    process_node!(adjacency_list, idx_to_idx, map, start_node, goal_node, start_node)
+
+    display(length(adjacency_list))
 
     # DFS
-    path_mask = falses(size(map)...)
-    dfs_on_compressed_graph(path_mask, start_node, 0, goal_node, adjacency_list)
+    path_mask = fill(false, length(adjacency_list))
+    dfs_on_compressed_graph(path_mask, idx_to_idx[start_node], 0, idx_to_idx[goal_node], adjacency_list)
 end
 
-function process_node!(adjacency_list, map, node, goal_node, parent_node)
+function add_child(adjacency_list, idx_to_idx, parent_node, child_node, path_len)
+    for node in (parent_node, child_node)
+        if !haskey(idx_to_idx, node)
+            idx_to_idx[node] = length(adjacency_list)+1
+            push!(adjacency_list, Tuple((1, 0) for _ in 1:4))
+        end
+    end
+    idx = idx_to_idx[parent_node]
+    next_free_idx = findfirst(==((1, 0)), adjacency_list[idx])
+    new_tuple = Base.setindex(adjacency_list[idx], (idx_to_idx[child_node], path_len), next_free_idx)
+    adjacency_list[idx] = new_tuple
+end
+
+function process_node!(adjacency_list, idx_to_idx, map, node, goal_node, parent_node)
     path_len = node != parent_node
 
     while true
@@ -101,7 +117,7 @@ function process_node!(adjacency_list, map, node, goal_node, parent_node)
         path_len += 1
 
         if node == goal_node
-            push!(get!(adjacency_list, parent_node, []), (node..., path_len-1))
+            add_child(adjacency_list, idx_to_idx, parent_node, node, path_len-1)
             break
         end
 
@@ -114,8 +130,8 @@ function process_node!(adjacency_list, map, node, goal_node, parent_node)
             possible_paths += map[next_node...] == '.'
 
             if map[next_node...] == 'B'
-                push!(get!(adjacency_list, parent_node, []), (next_node..., path_len))
-                push!(get!(adjacency_list, next_node, []), (parent_node..., path_len))
+                add_child(adjacency_list, idx_to_idx, parent_node, next_node, path_len)
+                add_child(adjacency_list, idx_to_idx, next_node, parent_node, path_len)
             end
         end
 
@@ -134,15 +150,15 @@ function process_node!(adjacency_list, map, node, goal_node, parent_node)
         else
             map[node...] = 'B'
 
-            push!(get!(adjacency_list, node, []), (parent_node..., path_len-1))
-            push!(get!(adjacency_list, parent_node, []), (node..., path_len-1))
+            add_child(adjacency_list, idx_to_idx, parent_node, node, path_len-1)
+            add_child(adjacency_list, idx_to_idx, node, parent_node, path_len-1)
 
             for dir in DIRS
                 next_node = node .+ dir
                 checkbounds(Bool, map, next_node...) || continue
 
                 if map[next_node...] == '.'
-                    process_node!(adjacency_list, map, next_node, goal_node, node)
+                    process_node!(adjacency_list, idx_to_idx, map, next_node, goal_node, node)
                 end
             end
 
@@ -153,34 +169,19 @@ end
 
 function dfs_on_compressed_graph(path_mask, node, path_length, goal_node, adjacency_list)
     node == goal_node && return path_length
-    checkbounds(Bool, path_mask, node...) || return 0
 
-    @inbounds path_mask[node...] = true
+    @inbounds path_mask[node] = true
     max_len = 0
-    for child in adjacency_list[node]
-        child_node = child[1:2]
-        @inbounds path_mask[child_node...] && continue
+    for child in @inbounds adjacency_list[node]
+        @inbounds child_node = child[1]
+        @inbounds path_mask[child_node] && continue
 
-        max_len = max(max_len, dfs_on_compressed_graph(path_mask, child_node, path_length + child[3], goal_node, adjacency_list))
+        @inbounds max_len = max(max_len, dfs_on_compressed_graph(path_mask, child_node, path_length + child[2], goal_node, adjacency_list))
     end
-    @inbounds path_mask[node...] = false
+    @inbounds path_mask[node] = false
 
     return max_len
 end
-
-# FxHash
-function Base.hash(t::NTuple{N, Int}) where N
-    h = 0
-
-    for a in t
-        h = bitrotate(h, 5)
-        h ⊻= reinterpret(UInt, a)
-        h *= 0x517cc1b727220a95
-    end
-
-    h
-end
-
 
 using Test
 @test part2("""
